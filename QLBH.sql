be5866aa8f0baefe7867fda9134e473bb46cffc0
Kiet <21521034@gm.uit.edu.vn>
2022-12-05 17:19:10 +0700

    LAB 5
    trigger

--- QLBH.sql
+++ QLBH.sql
@@ -174,6 +174,7 @@ INSERT INTO CTHD VALUES(1022,'ST07',1)
 INSERT INTO CTHD VALUES(1023,'ST04',6)
 
 --Tuan 1
+-- Cau 1. Tạo các quan hệ và khai báo các khóa chính, khóa ngoại của quan hệ.
 ALTER TABLE SANPHAM ADD GHICHU varchar(20)
 ALTER TABLE KHACHHANG ADD LOAIKH tinyint
 ALTER TABLE SANPHAM ALTER COLUMN GHICHU varchar(100)
@@ -184,68 +185,74 @@ ALTER TABLE SANPHAM ADD CONSTRAINT SANPHAM_GIA CHECK (GIA > 500)
 ALTER TABLE CTHD ADD CONSTRAINT CTHD_SL CHECK (SL>=1)
 ALTER TABLE KHACHHANG ADD CONSTRAINT KHACHHANG_NGDK_NGSINH CHECK (NGDK > NGSINH)
 
- --Tuan 2
- --Phan 2 Cau 2 -> 5 
+--Tuan 2
+
+--Phan 2 Cau 2 -> 5 
+-- Cau 2. Tạo quan hệ SANPHAM1 chứa toàn bộ dữ liệu của quan hệ SANPHAM. Tạo quan hệ KHACHHANG1 chứa toàn bộ dữ liệu của quan hệ KHACHHANG. 
 SELECT * INTO SANPHAM1 FROM SANPHAM
 SELECT * INTO KHACHHANG1 FROM KHACHHANG
+-- Cau 3. Cập nhật giá tăng 5% đối với những sản phẩm do “Thai Lan” sản xuất (cho quan hệ SANPHAM1)
 UPDATE SANPHAM1 
 SET GIA = GIA + GIA*(5/100) 
 WHERE NUOCSX = 'Thai Lan'
+-- Cau 4. Cập nhật giá giảm 5% đối với những sản phẩm do “Trung Quoc” sản xuất có giá từ 10.000 trở xuống (cho quan hệ SANPHAM1).
 UPDATE SANPHAM1 
 SET GIA = GIA - GIA*(5/100) 
 WHERE NUOCSX = 'Trung Quoc' AND GIA < 10000
+-- Cau 5. Cập nhật giá trị LOAIKH là “Vip” đối với những khách hàng đăng ký thành viên trước ngày 1/1/2007 có doanh số từ 10.000.000 trở lên hoặc khách hàng đăng ký thành viên từ 1/1/2007 trở về sau có doanh số từ 2.000.000 trở lên (cho quan hệ KHACHHANG1).
 UPDATE KHACHHANG1 
 SET LOAIKH = 'VIP' 
 WHERE (NGDK < '1/1/2007' AND DOANHSO > 10000000) 
 OR (NGDK > '1/1/2007' AND DOANHSO > 2000000)
+
 --Phan 3 Cau 1 -> 11
--- Cau 1
+-- Cau 1. In ra danh sách các sản phẩm (MASP,TENSP) do “Trung Quoc” sản xuất.
 SELECT MASP, TENSP 
 FROM SANPHAM 
 WHERE NUOCSX = 'Trung Quoc'
--- Cau 2
+-- Cau 2. In ra danh sách các sản phẩm (MASP, TENSP) có đơn vị tính là “cay”, ”quyen”.
 SELECT MASP, TENSP 
 FROM SANPHAM 
 WHERE (DVT = 'cay' or DVT = 'quyen')
--- Cau 3
+-- Cau 3. In ra danh sách các sản phẩm (MASP,TENSP) có mã sản phẩm bắt đầu là “B” và kết thúc là “01”.
 SELECT MASP, TENSP 
 FROM SANPHAM 
 WHERE MASP LIKE 'B%01'
--- Cau 4
+-- Cau 4. In ra danh sách các sản phẩm (MASP,TENSP) do “Trung Quốc” sản xuất có giá từ 30.000 đến 40.000.
 SELECT MASP, TENSP 
 FROM SANPHAM 
 WHERE (NUOCSX = 'Trung Quoc') AND (GIA BETWEEN 30000 AND 40000)
--- Cau 5
+-- Cau 5. In ra danh sách các sản phẩm (MASP,TENSP) do “Trung Quoc” hoặc “Thai Lan” sản xuất có giá từ 30.000 đến 40.000.
 SELECT MASP, TENSP 
 FROM SANPHAM 
 WHERE 
     (NUOCSX = 'Trung Quoc' OR NUOCSX = 'Thai Lan') 
     AND (GIA BETWEEN 30000 AND 40000)
--- Cau 6
+-- Cau 6. In ra các số hóa đơn, trị giá hóa đơn bán ra trong ngày 1/1/2007 và ngày 2/1/2007.
 SELECT SOHD, TRIGIA 
 FROM HOADON 
 WHERE 
     (NGHD = '1/1/2007' OR NGHD = '2/1/2007')
--- Cau 7
+-- Cau 7. In ra các số hóa đơn, trị giá hóa đơn trong tháng 1/2007, sắp xếp theo ngày (tăng dần) và trị giá của hóa đơn (giảm dần).
 SELECT SOHD, TRIGIA 
 FROM HOADON 
 WHERE 
     (MONTH(NGHD) = 1 AND YEAR(NGHD) = 2007) 
 ORDER BY NGHD ASC, TRIGIA DESC
--- Cau 8
+-- Cau 8. In ra danh sách các khách hàng (MAKH, HOTEN) đã mua hàng trong ngày 1/1/2007.
 SELECT KHACHHANG.MAKH, HOTEN 
 FROM KHACHHANG, HOADON 
 WHERE 
     KHACHHANG.MAKH = HOADON.MAKH 
     AND HOADON.NGHD = '1/1/2007'
--- Cau 9
+-- Cau 9. In ra số hóa đơn, trị giá các hóa đơn do nhân viên có tên “Nguyen Van B” lập trong ngày 28/10/2006.
 SELECT SOHD, TRIGIA 
 FROM NHANVIEN, HOADON 
 WHERE 
     NHANVIEN.HOTEN = 'Nguyen Van B' 
     AND HOADON.MANV = NHANVIEN.MANV 
     AND NGHD = '28/10/2006'
--- Cau 10
+-- Cau 10. In ra danh sách các sản phẩm (MASP,TENSP) được khách hàng có tên “Nguyen Van A” mua trong tháng 10/2006.
 SELECT distinct SANPHAM.MASP, TENSP 
 FROM KHACHHANG, HOADON, SANPHAM, CTHD
 WHERE 
@@ -255,39 +262,39 @@ WHERE
     AND KHACHHANG.HOTEN = 'Nguyen Van A' 
     AND YEAR(NGHD) = 2006 
     AND MONTH(NGHD) = 10
--- Cau 11
+-- Cau 11. Tìm các số hóa đơn đã mua sản phẩm có mã số “BB01” hoặc “BB02”.
 SELECT SOHD 
 FROM CTHD 
 WHERE MASP IN ('BB01', 'BB02')
 
 --Tuan 3
 -- Phan 3 cau 12 - 13
--- Cau 12
+-- Cau 12. Tìm các số hóa đơn đã mua sản phẩm có mã số “BB01” hoặc “BB02”, mỗi sản phẩm mua với số lượng từ 10 đến 20.
 SELECT SOHD 
 FROM CTHD 
 WHERE 
     MASP IN ('BB01', 'BB02') 
     AND (SL BETWEEN 10 AND 20)
--- Cau 13
+-- Cau 13. Tìm các số hóa đơn mua cùng lúc 2 sản phẩm có mã số “BB01” và “BB02”, mỗi sản phẩm mua với số lượng từ 10 đến 20.
 SELECT SOHD 
 FROM CTHD 
 WHERE 
     (MASP = 'BB01' AND MASP = 'BB02') 
     AND (SL BETWEEN 10 AND 20)
 -- Phan 3 cau 14 - 19
--- Cau 14
+-- Cau 14. In ra danh sách các sản phẩm (MASP,TENSP) do “Trung Quoc” sản xuất hoặc các sản phẩm được bán ra trong ngày 1/1/2007.
 SELECT distinct SANPHAM.MASP, TENSP 
 FROM SANPHAM, HOADON, CTHD 
 WHERE 
     SANPHAM.MASP = CTHD.MASP 
     AND HOADON.SOHD = CTHD.SOHD 
     and NUOCSX ='Trung Quoc' OR HOADON.NGHD = '1/1/2007'
--- Cau 15
+-- Cau 15. In ra danh sách các sản phẩm (MASP,TENSP) không bán được.
 SELECT MASP, TENSP 
 FROM SANPHAM 
 WHERE 
     MASP NOT IN (SELECT MASP FROM CTHD)
--- Cau 16
+-- Cau 16. In ra danh sách các sản phẩm (MASP,TENSP) không bán được trong năm 2006.
 SELECT MASP, TENSP 
 FROM SANPHAM 
 WHERE 
@@ -297,7 +304,7 @@ WHERE
         WHERE CTHD.SOHD = HOADON.SOHD
         AND YEAR(NGHD) = 2006
         )
--- Cau 17
+-- Cau 17. In ra danh sách các sản phẩm (MASP,TENSP) do “Trung Quoc” sản xuất không bán được trong năm 2006.
 SELECT MASP, TENSP 
 FROM SANPHAM 
 WHERE 
@@ -307,7 +314,7 @@ WHERE
         WHERE CTHD.SOHD = HOADON.SOHD
         AND YEAR(NGHD) = 2006
         )
--- Cau 18
+-- Cau 18. Tìm số hóa đơn đã mua tất cả các sản phẩm do Singapore sản xuất.
 SELECT SOHD 
 FROM HOADON
 WHERE NOT EXISTS
@@ -322,7 +329,7 @@ WHERE NOT EXISTS
 		    AND CTHD.MASP = SANPHAM.MASP
 	    )
     )
--- Cau 19
+-- Cau 19. Tìm số hóa đơn trong năm 2006 đã mua ít nhất tất cả các sản phẩm do Singapore sản xuất.
 SELECT SOHD 
 FROM HOADON
 WHERE NOT EXISTS
@@ -340,36 +347,36 @@ WHERE NOT EXISTS
     )
 
 -- Tuan 4 cau 20 - 30
- -- Cau 20
+-- Cau 20. Có bao nhiêu hóa đơn không phải của khách hàng đăng ký thành viên mua?
 select count(*)
 from HOADON
 where 
     MAKH is NULL
- -- Cau 21
+-- Cau 21. Có bao nhiêu sản phẩm khác nhau được bán ra trong năm 2006.
 select count(distinct MASP)
 from HOADON, CTHD
 where 
 	HOADON.SOHD = CTHD.SOHD
 	and year(NGHD) = 2006
--- Cau 22
+-- Cau 22. Cho biết trị giá hóa đơn cao nhất, thấp nhất là bao nhiêu ?
 select max(TRIGIA) max_trigia, min (TRIGIA) min_trigia 
 from HOADON
--- Cau 23
+-- Cau 23. Trị giá trung bình của tất cả các hóa đơn được bán ra trong năm 2006 là bao nhiêu?
 select avg(TRIGIA)
 from HOADON 
 where 
     year(NGHD) = 2006
--- Cau 24
+-- Cau 24. Tính doanh thu bán hàng trong năm 2006.
 select sum(TRIGIA)
 from HOADON
 where 
     year(NGHD)= 2006
--- Cau 25
+-- Cau 25. Tìm số hóa đơn có trị giá cao nhất trong năm 2006.
 select max(TRIGIA)
 from HOADON
 where 
     year(NGHD) = 2006
--- Cau 26
+-- Cau 26. Tìm họ tên khách hàng đã mua hóa đơn có trị giá cao nhất trong năm 2006.
 select distinct HOTEN
 from KHACHHANG, HOADON
 WHERE 
@@ -377,14 +384,18 @@ WHERE
 	AND YEAR(NGHD) = 2006
 	AND TriGia >= all 
         (
+<<<<<<< Updated upstream
         SELECT TRIGIA 
+=======
+        SELECT MAX(TRIGIA) 
+>>>>>>> Stashed changes
         FROM HoaDon WHERE YEAR(NGHD) = 2006
         )
--- Cau 27
+-- Cau 27. In ra danh sách 3 khách hàng (MAKH, HOTEN) có doanh số cao nhất.
 select top 3 MAKH, HOTEN
 from KHACHHANG
 order by DOANHSO desc
--- Cau 28
+-- Cau 28. In ra danh sách các sản phẩm (MASP, TENSP) có giá bán bằng 1 trong 3 mức giá cao nhất.
 select MASP, TENSP
 from SANPHAM
 where GIA in
@@ -393,7 +404,7 @@ where GIA in
 	from SANPHAM
 	order by GIA desc
 	)
--- Cau 29
+-- Cau 29. In ra danh sách các sản phẩm (MASP, TENSP) do “Thai Lan” sản xuất có giá bằng 1 trong 3 mức giá cao nhất (của tất cả các sản phẩm).
 select MASP, TENSP
 from SANPHAM
 where 
@@ -404,7 +415,7 @@ where
 	    order by GIA desc
 	    )
 	and NUOCSX = 'Thai Lan'
--- Cau 30
+-- Cau 30. In ra danh sách các sản phẩm (MASP, TENSP) do “Trung Quoc” sản xuất có giá bằng 1 trong 3 mức giá cao nhất (của sản phẩm do “Trung Quoc” sản xuất).
 select MASP, TENSP
 from SANPHAM
 where 
@@ -416,28 +427,28 @@ where
 	    order by GIA desc
 	    )
 	and NUOCSX = 'Trung Quoc'
--- Cau 31
+-- Cau 31. * In ra danh sách 3 khách hàng có doanh số cao nhất (sắp xếp theo kiểu xếp hạng).
 select top 3 *
 from KHACHHANG
 order by DOANHSO desc
--- Cau 32
+-- Cau 32. Tính tổng số sản phẩm do “Trung Quoc” sản xuất.
 select count(*)
 from SANPHAM
 where 
     NUOCSX = 'Trung Quoc'
--- Cau 33
+-- Cau 33. Tính tổng số sản phẩm của từng nước sản xuất.
 select NUOCSX, count(*) SOSP
 from SANPHAM
 group by NUOCSX
--- Cau 34
+-- Cau 34. Với từng nước sản xuất, tìm giá bán cao nhất, thấp nhất, trung bình của các sản phẩm.
 select NUOCSX, max(GIA) Max_GIA, min(GIA) Min_GIA, avg(GIA) Avg_Gia
 from SANPHAM
 group by NUOCSX
--- Cau 35
+-- Cau 35. Tính doanh thu bán hàng mỗi ngày.
 select NGHD, sum(TRIGIA) DOANHTHU
 from HOADON
 group by NGHD
--- Cau 36
+-- Cau 36. Tính tổng số lượng của từng sản phẩm bán ra trong tháng 10/2006.
 select SANPHAM.MASP, sum(SL) SOLUONGBANRA
 from SANPHAM, HOADON, CTHD
 where 
@@ -445,17 +456,17 @@ where
 	and HOADON.SOHD = CTHD.SOHD
 	and month(NGHD) = 10 and year (NGHD) = 2006
 group by SANPHAM.MASP
--- Cau 37
+-- Cau 37. Tính doanh thu bán hàng của từng tháng trong năm 2006.
 select month(NGHD) THANG, sum(TRIGIA) DOANHTHU
 from HOADON
 where year(NGHD) = 2006
 group by month(NGHD)
--- Cau 38
+-- Cau 38. Tìm hóa đơn có mua ít nhất 4 sản phẩm khác nhau.
 select SOHD 
 from CTHD
 group by SOHD
 having count(distinct CTHD.MASP) >= 4
--- Cau 39
+-- Cau 39. Tìm hóa đơn có mua 3 sản phẩm do “Viet Nam” sản xuất (3 sản phẩm khác nhau).
 select SOHD
 from CTHD, SANPHAM
 where 
@@ -463,13 +474,13 @@ where
 	and NUOCSX = 'Viet Nam'
 group by SOHD
 having count(distinct CTHD.MASP) >= 3
--- Cau 40
+-- Cau 40. Tìm khách hàng (MAKH, HOTEN) có số lần mua hàng nhiều nhất.
 select KHACHHANG.MAKH, HOTEN
 from KHACHHANG, HOADON
 where KHACHHANG.MAKH = HOADON.MAKH
 group by KHACHHANG.MAKH, HOTEN
 having count(*) >= all(select count(*) from HOADON group by MAKH)
--- Cau 41
+-- Cau 41. Tháng mấy trong năm 2006, doanh số bán hàng cao nhất ?
 select month(NGHD)
 from HOADON
 where year(NGHD) = 2006
@@ -482,7 +493,7 @@ having sum(TRIGIA) >= all
         year(NGHD) = 2006  
     group by month(NGHD)
     )
--- Cau 42
+-- Cau 42. Tìm sản phẩm (MASP, TENSP) có tổng số lượng bán ra thấp nhất trong năm 2006.
 select top 1 with ties SANPHAM.MASP, TENSP
 from SANPHAM, CTHD, HOADON
 where 
@@ -491,7 +502,7 @@ where
     and year(NGHD) = 2006
 group by SANPHAM.MASP, TENSP
 order by sum(SL)
--- Cau 43
+-- Cau 43. *Mỗi nước sản xuất, tìm sản phẩm (MASP,TENSP) có giá bán cao nhất.
 select NUOCSX, MASP, TENSP
 from SANPHAM S1
 where exists
@@ -503,12 +514,12 @@ where exists
         S1.NUOCSX = S2.NUOCSX
         and S1.GIA = max(GIA)
     ) 
--- Cau 44
+-- Cau 44. Tìm nước sản xuất sản xuất ít nhất 3 sản phẩm có giá bán khác nhau.
 select NUOCSX
 from SANPHAM
 group by NUOCSX
 having count(distinct GIA) > 3
--- Cau 45
+-- Cau 45. *Trong 10 khách hàng có doanh số cao nhất, tìm khách hàng có số lần mua hàng nhiều nhất.
 select *
 from KHACHHANG
 where MAKH in
@@ -525,5 +536,189 @@ where MAKH in
     order by count(*) desc
     )
     
+-- Tuan 5
+-- Cau 11 - 14
+-- Cau 11. Ngày mua hàng (NGHD) của một khách hàng thành viên sẽ lớn hơn hoặc bằng ngày khách hàng đó đăng ký thành viên (NGDK).
+-- trigger: them, sua ngayhd cua hoadon
+create trigger trg_ins_upd_NGHD 
+on HOADON
+for insert, update
+as
+begin
+	if (exists(select * from KHACHHANG kh, inserted i
+					where kh.MAKH = i.MAKH
+                    and kh.NGDK > i.NGHD))
+    begin 
+        print 'NGHD phai >= NGDK'
+        rollback transaction
+    end
+end
+-- Cach 2
+create trigger trg_ins_upd_NGHD
+on HOADON
+for update, insert
+as
+begin
+    declare @nghd smalldatetime,
+            @ngdk smalldatetime
+    select @nghd = NGHD, ngdk = NGDK
+    from KHACHHANG, inserted
+    where
+        inserted.MAKH = KHACHHANG.MAKH
+    if (@nghd < @ngdk) 
+    begin
+        print 'nghd phai lon hon ngdk'
+        rollback transaction
+    end
+end
 
+-- Cau 12. Ngày bán hàng (NGHD) của một nhân viên phải lớn hơn hoặc bằng ngày nhân viên đó vào làm.
+-- Them them, sua nghd cua hoadon
+create trigger trg_ins_upd_NGHD_VL
+on HOADON
+for insert, update
+as
+begin 
+    if (exists(select * from NHANVIEN nv, inserted i
+                    where nv.MANV = i.MANV
+                    and nv.NGVL > i.NGHD ))
+    begin
+        print 'Ngay vao lam phai < = nghd'
+        rollback transaction
+    end
+end
+-- Cach 2
+create trigger trg_ins_upd_NGHD_VL
+on HOADON
+for insert, update
+as
+begin
+    declare @nghd smalldatetime,
+            @ngvl smalldatetime
+    select @nghd = NGHD, @ngvl = NGVL
+    from NHANVIEN, inserted
+    where
+        inserted.MANV = NHANVIEN.MANV
+    if (@nghd < @ngvl)
+    begin
+        print 'Nghd phai lon hon ngay vao lam'
+        rollback transaction
+    end
+end
+-- Cau 13. Mỗi một hóa đơn phải có ít nhất một chi tiết hóa đơn.
+-- Xoa cthd 
+create trigger trg_del_CTHD
+on CTHD
+for delete
+as
+begin
+    declare @sl tinyint,
+            @sohd int
+    select @sl = count(cthd.SOHD), @sohd = d.SOHD
+    from deleted d, CTHD cthd
+    where 
+        cthd.SOHD = d.SOHD
+    group by d.SOHD
+    if (@sl < 1)
+    begin
+        print 'Phai co it nhat 1 cthd'
+        rollback transaction
+    end
+end
+
+-- Cau 14. Trị giá của một hóa đơn là tổng thành tiền (số lượng*đơn giá) của các chi tiết thuộc hóa đơn đó.
+-- Them HOADON tri gia = 0
+create trigger trg_ins_HOADON
+on HOADON
+for insert
+as
+begin
+    update HOADON
+    set TRIGIA = 0
+    where 
+        SOHD = (select SOHD from inserted)
+    
+    print 'Da tao 1 hoa don voi gia tri hoa don ban dau la 0'
+end
+
+-- Update Hoadon
+create trigger trg_upd_HOADON
+on HOADON
+for update
+as
+begin
+    update HOADON
+    set TRIGIA = (select TRIGIA from deleted)
+    where SOHD = (select SOHD from inserted)
+    print 'update 1 hoa don voi gia tri khong doi'
+end
+
+-- Them cthd
+create trigger trg_ins_CTHD
+on CTHD
+for insert
+as
+begin
+    declare @sl tinyint,
+            @gia money,
+            @sohd int
+    select @gia = GIA, @sohd = SOHD, @sl = SL
+    from inserted i, SANPHAM sp
+    where
+        i.MASP = sp.MASP
+    begin
+        update HOADON
+        set TRIGIA += @sl*@gia
+        print 'Da them 1 cthd va sua tri gia hoa don tuong ung'
+    end
+end
+
+-- Xoa cthd
+create trigger trg_del_CTHD
+on CTHD
+for delete
+as
+begin
+    declare @sl tinyint,
+            @gia money,
+            @sohd int
+    select @gia = GIA, @sohd = SOHD, @sl = SL
+    from deleted d, SANPHAM sp
+    where
+        d.MASP = sp.MASP
+    begin
+        update HOADON
+        set TRIGIA -= @sl*@gia
+        print 'Da xoa 1 cthd va  sua tri gia hoa don tuong ung'
+    end
+end
+
+-- Sua cthd
+create trigger trg_upd_CTHD
+on CTHD
+for delete
+as
+begin
+    declare @sl_cu tinyint,
+            @sl_moi tinyint,
+            @gia money,
+            @sohd_cu int,
+            @sohd_moi int
+    select @gia = GIA, @sohd_cu = SOHD, @sl_cu = SL
+    from deleted d, SANPHAM sp
+    where
+        d.MASP = sp.MASP
+    
+    select @gia = GIA, @sohd_moi = SOHD, @sl_moi = SL
+    from inserted i, SANPHAM sp
+    where
+        i.MASP = sp.MASP
+    
+    if (@sohd_cu = @sohd_moi)
+    begin
+        update HOADON
+        set TRIGIA = TRIGIA - @sl_cu*@gia + @sl_moi*@gia
+        print 'Da sua 1 cthd va  sua tri gia hoa don tuong ung'
+    end
+end
 
